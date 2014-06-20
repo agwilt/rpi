588119e8eab5d25444ed7b04c3940b11eaf58242
Andreas Gwilt <andreasgwilt@gmail.com>
2014-06-20 13:36:09 +0000

    Spcify tones, not freqs

--- sound.py
+++ sound.py
@@ -4,6 +4,32 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
+def note_to_freq(note,a=440.0):
+	notes = { 'a':9,
+		'b':11,
+		'c':0,
+		'd':2,
+		'e':4,
+		'f':5,
+		'g':7,
+		}
+	c = a * (2**(1/12.0))**(-57) # c0
+
+	f = notes[note[0]]
+
+	if "is" in note:
+		f += 1
+		n = int(note[3:])
+	elif "es" in note:
+		f -= 1
+		n = int(note[3:])
+	else:
+		n = int(note[1:])
+
+	print c * (2**(1/12.0))**(f+(n*12))
+	return c * (2**(1/12.0))**(f+(n*12))
+	
+
 def init():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(25, GPIO.OUT)
@@ -21,8 +47,9 @@ def play(music):
 	T = 240 / float(music.pop(0)) # time in seconds for a bar
 	for note in music:
 		if note[0] != '/':
-			c = note.index(",")
-			freq = float(note[:c])
+			c = note.index(" ")
+			print("Note: %s" % note[:c])
+			freq = note_to_freq(note[:c])
 			length = T/float(note[c+1:])
 			sound(length, freq)
 
