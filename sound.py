393c227178cd983d0e6b9d7d58181ab053197734
Andreas Gwilt <andreasgwilt@gmail.com>
2014-06-20 12:01:57 +0000

    Try to change to tempo

--- sound.py
+++ sound.py
@@ -18,14 +18,16 @@ def sound(length, freq):
 		time.sleep(i - 0.00018)
 
 def play(music):
+	T = 240 / int(music.pop(0)) # time in seconds for a bar
 	for note in music:
 		if note[0] != '/':
 			c = note.index(",")
-			freq = int(note[:c])
-			time = float(note[c+1:])
+			freq = float(note[:c])
+			time = T/int(note[c+1:])
 			sound(time, freq)
 
 if __name__ == "__main__":
 	init()
-	music = open(sys.argv[1]).read().split()
+	music = open(sys.argv[1]).read().split("\n")
+	music.pop()
 	play(music)
